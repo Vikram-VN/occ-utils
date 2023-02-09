c8ff339d91909830bae5d11d73ecaba06f4de19d
vikram-N68 <vikram.narayanappa@apexit.com>
2023-02-09 15:17:38 +0530

    added comments

--- excelToJson.js
+++ excelToJson.js
@@ -3,7 +3,7 @@ node_xj(
 	{
 		input: "recommended spare parts lists as of 20230110.xlsx", // input xls
 		output: "output.json", // output json
-		sheet: "Refuse RSP", // specific sheetname
+		sheet: "Refuse RSP", // specific sheet name
 		// rowsToSkip: 5, // number of rows to skip at the top of the sheet; defaults to 0
 		allowEmptyKey: false, // avoids empty keys in the output, example: {"": "something"}; default: true
 	},
