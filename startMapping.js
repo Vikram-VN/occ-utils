921cf4ad8397b193d952b6f718aec47231fb6e50
vikram-N68 <vikram.narayanappa@apexit.com>
2023-02-10 15:32:00 +0530

    enhanced logic

--- startMapping.js
+++ startMapping.js
@@ -1,19 +1,20 @@
 const axios = require("axios");
 const fs = require("fs");
 
-const INSTANCE_URL = "https://test-admin.occa.ocs.oraclecloud.com"; //Change the link with your instance link
-const AUTH_TOKEN = ""; // Authorization token 
+const INSTANCE_URL = "https://p11281411c1tst-admin.occa.ocs.oraclecloud.com"; //Change the link with your instance link
+const AUTH_TOKEN = "eyJhbGciOiJSUzI1NiIsImprdSI6InAxMTI4MTQxMWMxVFNUIiwia2lkIjoiMCIsIng1YyI6bnVsbCwieDV1IjoiaHR0cHM6Ly9wMTEyODE0MTFjMXRzdC1hZG1pbi5vY2NhLm9jcy5vcmFjbGVjbG91ZC5jb20vY2NhZG1pbi92MS90ZW5hbnRDZXJ0Q2hhaW4ifQ==.eyJpYXQiOjE2NzYwMjIxNjIsImV4cCI6MTY3NjAyNTc2Miwic3ViIjoidmlrcmFtLm5hcmF5YW5hcHBhQGFwZXhpdC5jb20iLCJhdWQiOiJhZG1pblVJIiwiY29tLm9yYWNsZS5hdGcuY2xvdWQuY29tbWVyY2Uucm9sZXMiOlsiYWRtaW5Sb2xlIl0sIm9jY3MuYWRtaW4ucm9sZXMiOlsiYWRtaW5Sb2xlIl0sImlzcyI6Imh0dHBzOi8vcDExMjgxNDExYzF0c3QtYWRtaW4ub2NjYS5vY3Mub3JhY2xlY2xvdWQuY29tL29jY3MtYWRtaW4iLCJvY2NzLmFkbWluLmxvY2FsZSI6ImVuX1VTIiwib2Njcy5hZG1pbi50eiI6bnVsbCwib2Njcy5hZG1pbi50ZW5hbnRUeiI6IkV0Yy9VVEMiLCJvY2NzLmFkbWluLmtlZXBBbGl2ZVVSTCI6Imh0dHBzOi8vcDExMjgxNDExYzF0c3QtYWRtaW4ub2NjYS5vY3Mub3JhY2xlY2xvdWQuY29tL2tlZXBhbGl2ZSIsIm9jY3MuYWRtaW4udG9rZW5SZWZyZXNoVVJMIjoiaHR0cHM6Ly9wMTEyODE0MTFjMXRzdC1hZG1pbi5vY2NhLm9jcy5vcmFjbGVjbG91ZC5jb20vY2NhZG1pbi92MS9zc29Ub2tlbnMvcmVmcmVzaCIsIm9jY3MuYWRtaW4udmVyc2lvbiI6IjIyLjQuMiIsIm9jY3MuYWRtaW4uYnVpbGQiOiJqZW5raW5zLUFzc2VtYmxlX0Nsb3VkX0NvbW1lcmNlX0VBUnNfLW1hc3Rlci0yNjUiLCJvY2NzLmFkbWluLmVtYWlsIjoidmlrcmFtLm5hcmF5YW5hcHBhQGFwZXhpdC5jb20iLCJvY2NzLmFkbWluLnByb2ZpbGVJZCI6Iml1c2VyMzU0NjgwIiwib2Njcy5hZ2VudC5vYm8iOm51bGwsIm9jY3MuYWRtaW4uZmlyc3ROYW1lIjoiVmlrcmFtIiwib2Njcy5hZG1pbi5sYXN0TmFtZSI6Im5hcmF5YW5hcHBhIiwib2Njcy5hZG1pbi5wdW5jaG91dFVzZXIiOmZhbHNlLCJzdWJfdHlwZSI6bnVsbCwic2NvcGUiOm51bGx9.YFHZSA24Eu+lU36lkP1CPXz/p9iNhJ0gnsOGFYdlwZwcm3JhziSmWrL+yiDiZSacOFjSQMfV/3adNOVjjyV56Q1WwoRpYcNQMZNTQnfpy91oDLxX1CZAwoO7M96gtOGKEYn5imNyqjmdm5CITS5bMrQyHe1WzFVErQq+YNOegGBsYnNVSW2vTh7pgo6MXEV4Y8eW+n3z0TV+NYfX/b9Oj7XspUGLPhQlED0BcZRNPl25FNkDjwWmhglrZTUlXcECgT0szywWiL3vOixi758Kzeco3NLvzbNNBeG8Oxy+l5YomUK3UXtcCAx0YcXAeMRnyZhe9ttlpkpqUiQ/JmFMVQ=="; // Authorization token 
 
 const startProductMapping = async (productId, tileId, type = 0, wait = false) => {
 
     const fields = ["x_refuseRSP", "x_mixerRSP"]; //Fields that you want to map
+    const catalogs = ["refuseCatalog", "mixerCatalog"];
 
     try {
 
         const getTiledata = await axios.request({
             method: "GET",
             baseURL: INSTANCE_URL,
-            url: `/ccadminui/v1/products/${productId}?includePrices=false&minimalEdit=true&showCatalogsInfo=true`,
+            url: `/ccadminui/v1/products/${productId}?includePrices=false&minimalEdit=true&showCatalogsInfo=true&catalogId=${catalogs[type]}`,
             headers: {
                 "content-type": "application/json",
                 "x-ccasset-language": "en",
@@ -32,7 +33,7 @@ const startProductMapping = async (productId, tileId, type = 0, wait = false) =>
         const setTileData = await axios.request({
             method: "PUT",
             baseURL: INSTANCE_URL,
-            url: `/ccadminui/v1/products/${productId}?includePrices=false`,
+            url: `/ccadminui/v1/products/${productId}?includePrices=false&catalogId=${catalogs[type]}`,
             data: {
                 properties: {
                     [fields[type]]: wait ? newPayload : ""
@@ -44,14 +45,14 @@ const startProductMapping = async (productId, tileId, type = 0, wait = false) =>
                 "Authorization": `Bearer ${AUTH_TOKEN}`
             }
         }).then(res => res.data);
-        const log = fields[type] + " -> " + productId + " -> " + setTileData[fields[type]] + "|" + INSTANCE_URL + "\r\n";
+        const log = "CatalogId -> " + catalogs[type] + " ->" + fields[type] + " -> " + productId + " -> " + setTileData[fields[type]]  + " -> " + INSTANCE_URL + "\r\n";
         setTileData[fields[type]] && fs.appendFileSync("success-list.log", log, { encoding: "utf-8" })
-        console.log(fields[type], " -> ", productId + " -> " + setTileData[fields[type]] + "\r\n");
+        console.log("CatalogId -> " + catalogs[type] + " ->" + fields[type] + " -> " + productId + " -> " + setTileData[fields[type]]  + " -> " + INSTANCE_URL + "\r\n");
 
     } catch (e) {
-        const log = fields[type] + " -> " + productId + " -> " + e.response?.data?.message + "|" + INSTANCE_URL + "\r\n"
+        const log = "CatalogId -> " + catalogs[type] + " ->" + fields[type] + " -> " + productId + " -> " + e.response?.data?.message +  " -> " + newPayload + " -> " + INSTANCE_URL + "\r\n";
         fs.appendFileSync("error-list.log", log, { encoding: "utf-8" })
-        console.log(fields[type], " -> ", productId + " -> " + e.response?.data?.message + "\r\n");
+        console.log("CatalogId -> " + catalogs[type] + " ->" + fields[type] + " -> " + productId + " -> " + e.response?.data?.message +  " -> " + newPayload + " -> " + INSTANCE_URL + "\r\n");
     }
 }
 
