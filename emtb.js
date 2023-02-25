32fd815e75f757752d61cc57ed84a17eaffe84f0
Vikram-N68 <vikram.narayanappa@apexit.com>
2023-02-25 22:49:34 +0530

    thank god, code is somehow working

--- emtb.js
+++ emtb.js
@@ -2,21 +2,21 @@ const fs = require("fs");
 
 const folders = fs.readdirSync("./templates", { encoding: "utf-8" });
 
-const dataTypes = {
-  array: {},
-  object: {},
-  items: [{}],
-  null: null,
-  integer: 0,
-  number: 0,
-  double: 0.0,
-  boolean: false,
-  string: ""
-}
-
-const listItems = ["items", "payments", "shippingAddress", "shippingMethod", "shippingPriceInfo", "storeAddress", "priceInfo"];
-
 const processData = (json, data, filtered = false, prevKey) => {
+  const dataTypes = {
+    array: {},
+    object: {},
+    items: [{}],
+    null: null,
+    integer: 0,
+    number: 0,
+    double: 0.0,
+    boolean: false,
+    string: ""
+  }
+
+  const listItems = ["items", "payments", "shippingAddress", "shippingMethod", "shippingPriceInfo", "storeAddress", "priceInfo"];
+
   let payload;
   if (!filtered) {
     // Converting data into array list regex
@@ -28,7 +28,8 @@ const processData = (json, data, filtered = false, prevKey) => {
   if (payload.length >= 2) {
     const dataType = payload[1].trim();
     const key = payload[0].trim();
-    const value = dataTypes[(listItems.includes(key) && dataType.toLowerCase() !== "string" ? "items" : dataType).toLowerCase()];
+    const find = listItems.includes(key) && dataType.toLowerCase() !== "string" ? "items" : dataType.toLowerCase();
+    const value = dataTypes[find];
     switch (dataType) {
       case "string":
       case "boolean":
@@ -60,18 +61,19 @@ const processData = (json, data, filtered = false, prevKey) => {
 
 const processEmailTemplate = data => {
   const newData = data.split("data.").slice(1);
-  // Deleting the json from memory to avoid duplicate data ( :) )
-  delete json;
   const json = {};
   // Converting string object into array list and iterating using map method
   newData.map(data => processData(json, data.replace(/\n/g, "")));
   return json; // Add return statement to return the processed JSON object
 }
-folders.slice(10,11).forEach((folder, index) => {
+folders.forEach((folder, index) => {
   const data = fs.readFileSync(`./templates/${folder}/Readme.txt`, { encoding: "utf-8" });
   try {
+    if (index === 10) {
+      console.log('ok');
+    }
     const jsonObject = processEmailTemplate(data);
-    console.log(index + ") " + folder, '\r\n')
+    console.log(index + ") " + folder, '\r\n');
     fs.writeFileSync(`./json/${folder}.json`, JSON.stringify(jsonObject, null, 4), { encoding: "utf-8" })
   } catch (e) {
     console.log(e)
