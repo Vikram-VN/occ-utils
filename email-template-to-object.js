add999d119b0cc3e001716383ed5bc3de2a258cd
Vikram-N68 <vikram.narayanappa@apexit.com>
2023-02-25 22:52:17 +0530

    code is working now

--- email-template-to-object.js
+++ email-template-to-object.js
@@ -130,6 +130,8 @@ The following fields can be referenced in the templates.
   data.payShippingInSecondaryCurrency (boolean)`;
 
 
+
+const processData = (json, data, filtered = false, prevKey) => {
   const dataTypes = {
     array: {},
     object: {},
@@ -141,51 +143,50 @@ The following fields can be referenced in the templates.
     boolean: false,
     string: ""
   }
-  
+
   const listItems = ["items", "payments", "shippingAddress", "shippingMethod", "shippingPriceInfo", "storeAddress", "priceInfo"];
-  
-  const processData = (json, data, filtered = false, prevKey) => {
-    let payload;
-    if (!filtered) {
-      // Converting data into array list regex
-      payload = data.split(/[()]/gi).filter(data => data.trim());
-    } else {
-      // Ignoring first regex because it is 2nd time loop, so need to do split.
-      payload = data.map(data => data.replace("->", "").trim());
-    }
-    if (payload.length >= 2) {
-      const dataType = payload[1].trim();
-      const key = payload[0].trim();
-      const value = dataTypes[(listItems.includes(key) && dataType.toLowerCase() !== "string" ? "items" : dataType).toLowerCase()];
-      switch (dataType) {
-        case "string":
-        case "boolean":
-        case "double":
-        case "number":
-        case "null":
-        case "integer":
-          if (!prevKey) {
-            // This will be used to add json object key values to the main object
-            json[key] = value;
-          } else {
-            // This used to add key values pairs to items list
-            json[0][key] = value;
-          }
-          // Again iterating to set key values for object
-          processData(json, payload.slice(2), true);
-          break;
-        default:
-          // Removing 2 items from the array list (because we already used those)
-          const newPayload = payload.slice(2);
-          if (listItems.includes(key)) {
-            processData(json[key] = value, newPayload, true, key);
-          } else {
-            processData(json[key] = value, newPayload, true);
-          }
-      }
+
+  let payload;
+  if (!filtered) {
+    // Converting data into array list regex
+    payload = data.split(/[()]/gi).filter(data => data.trim());
+  } else {
+    // Ignoring first regex because it is 2nd time loop, so need to do split.
+    payload = data.map(data => data.replace("->", "").trim());
+  }
+  if (payload.length >= 2) {
+    const dataType = payload[1].trim();
+    const key = payload[0].trim();
+    const value = dataTypes[(listItems.includes(key) && dataType.toLowerCase() !== "string" ? "items" : dataType).toLowerCase()];
+    switch (dataType) {
+      case "string":
+      case "boolean":
+      case "double":
+      case "number":
+      case "null":
+      case "integer":
+        if (!prevKey) {
+          // This will be used to add json object key values to the main object
+          json[key] = value;
+        } else {
+          // This used to add key values pairs to items list
+          json[0][key] = value;
+        }
+        // Again iterating to set key values for object
+        processData(json, payload.slice(2), true);
+        break;
+      default:
+        // Removing 2 items from the array list (because we already used those)
+        const newPayload = payload.slice(2);
+        if (listItems.includes(key)) {
+          processData(json[key] = value, newPayload, true, key);
+        } else {
+          processData(json[key] = value, newPayload, true);
+        }
     }
   }
-  
+}
+
 
 const processEmailTemplate = data => {
   const newData = data.split("data.").slice(1);
