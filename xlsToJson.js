c579b2091f6415cc2689fa02090e44aa457a3141
Vikram-VN <vikram.narayanappa@apexit.com>
2023-04-18 14:39:15 +0530

    ok

--- xlsToJson.js
+++ xlsToJson.js
@@ -3,7 +3,7 @@ const node_xj = require("xls-to-json");
 node_xj(
 	{
 		input: "accounts-prod.xlsx", // input xls
-		output: "accounts-mixer.json", // output json
+		output: "./data/accounts-mixer.json", // output json
 		sheet: "Mixer", // specific sheet name
 		// rowsToSkip: 5, // number of rows to skip at the top of the sheet; defaults to 0
 		allowEmptyKey: true, // avoids empty keys in the output, example: {"": "something"}; default: true
