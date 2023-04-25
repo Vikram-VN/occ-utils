75a698959fa7e0050ec0a1dd6bc6e016462be647
Vikram-VN <vikram.narayanappa@apexit.com>
2023-04-26 01:17:59 +0530

    updated code

--- accounts/getOrgs.js
+++ accounts/getOrgs.js
@@ -28,11 +28,8 @@ const httpCall = async (orgName) => {
 
 const list = JSON.parse(fs.readFileSync('./data/allOrganizationsData.json')).organization;
 
-[...new Set(list)].forEach((org, index) => {
-    setTimeout(async () => {
-        await httpCall(org.name);
-    }, index * 1000);
-
+[...new Set(list)].forEach(async (org) => {
+    await httpCall(org.name);
 })
 
 // console.log([...new Set(list.split(`
